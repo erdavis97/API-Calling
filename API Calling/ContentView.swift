59ee7c4e183e99b4578de962e31cd320f84d2f40
Ethan Davis <edavis@bsd220.org>
2024-03-14 09:46:30 -0500

    established baseline from cat facts

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var facts = [String]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(facts, id: \.self) { fact in
+                Text(fact)
+            }
+            navigationTitle("Cat Facts")
+                .toolbar {
+                    Button(action: {
+                        Task {
+                            await loadData()
+                        }
+                    }, label: {
+                        Image(systemName: "arrow.clockwise")
+                    })
+                }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        if let url = URL(string: "https://meowfacts.herokuapp.com/?count=20") {
+            if let (data, _) = try? await URLSession.shared.data(from: url) {
+                if let decodedResponse = try? JSONDecoder().decode(Facts.self, from: data) {
+                    facts = decodedResponse.facts
+                }
+            }
+        }
     }
 }
 
@@ -24,3 +46,13 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Facts: Identifiable, Codable {
+    var id = UUID()
+    var facts: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case facts = "data"
+    }
+}
+
