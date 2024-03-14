637338023ae68b0db0297e609831a3f70da5609e
Ethan Davis <edavis@bsd220.org>
2024-03-14 09:49:49 -0500

    added alert

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var facts = [String]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
             List(facts, id: \.self) { fact in
@@ -28,6 +29,11 @@ struct ContentView: View {
         .task {
             await loadData()
         }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text ("Loading Error"),
+                  message: Text("There was a problem loading your cat facts"),
+                  dismissButton: .default(Text("OK")))
+        }
     }
     
     func loadData() async {
@@ -35,9 +41,11 @@ struct ContentView: View {
             if let (data, _) = try? await URLSession.shared.data(from: url) {
                 if let decodedResponse = try? JSONDecoder().decode(Facts.self, from: data) {
                     facts = decodedResponse.facts
+                    return
                 }
             }
         }
+        showingAlert = true
     }
 }
 
